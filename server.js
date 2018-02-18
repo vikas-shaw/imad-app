cbf1f2135c7c0f84e8852ec741f208108e6b305e
vikas-shaw <vikkvmaithon@gmail.com>
2018-02-18 16:23:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,15 +81,11 @@ app.get('/ui/style.css', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-/*app.get('/:articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
   //articleName == article-one
   //articles[articleName] == {} content object for article one
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
-});*/
-
-app.get('/article-one', function (req, res) {
-    res.send(createTemplate(article-one));
 });
 
 app.get('/ui/main.js', function (req, res) {
