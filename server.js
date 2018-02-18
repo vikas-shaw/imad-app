bbc6c48b7d31f8b6c626e125a32693c453d48ade
vikas-shaw <vikkvmaithon@gmail.com>
2018-02-18 16:48:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,9 +86,9 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-var counter = 0;
+//var counter = 0;
 app.get('/counter', function (req, res) {
-    counter = counter + 1;
+  //  counter = counter + 1;
     res.send("hii there this is the counter page");
 });
 
