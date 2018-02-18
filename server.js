13ceca30260d9a7f9d66511c1069dcebef28b2d6
vikas-shaw <vikkvmaithon@gmail.com>
2018-02-18 16:33:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
-})
+});
 
 app.get('/ui/style.css', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
