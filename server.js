01d8a00b1b141a207bfc515b580b9dd35247ba65
vikas-shaw <vikkvmaithon@gmail.com>
2018-02-20 01:15:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,10 +90,6 @@ app.get('/counter', function (req, res) {
    res.send('hii there '); 
 });
 
-app.get('/hello', function (req, res) {
-    res.send(path.join('hii there'));
-});
-
 app.get('/ui/style.css', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
